0f74dc893a7db5378021a3de4c709ee625c8be06
Umberto81 <goffredo.gonnella@gail.com>
2022-03-15 17:53:40 +0000

    data.js added

---
+++ src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products:[
+        {
+            _id:1,
+            name: 'Nike Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            pice: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product',
+        },
+        {
+            _id:2,
+            name: 'Adidas Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            pice: 120,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'High quality product',
+        },
+        {
+            _id:3,
+            name: 'Lacoste Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            pice: 300,
+            brand: 'Lacoste',
+            rating: 2.5,
+            numReviews: 5,
+            description: 'High quality product',
+        },
+        {
+            _id:4,
+            name: 'Pants Adidas',
+            category: 'Pants',
+            image: './images/p1.jpg',
+            pice: 80,
+            brand: 'Adidas',
+            rating: 2.5,
+            numReviews: 3,
+            description: 'High quality product',
+        },
+        {
+            _id:5,
+            name: 'Puma',
+            category: 'Pants',
+            image: './images/p1.jpg',
+            pice: 80,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product',
+        },
+        {
+            _id:6,
+            name: 'Adidas Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            pice: 350,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'High quality product',
+        },
+
+    ]
+}
+
+export default data;
