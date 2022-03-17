31e7093dc879c5e8cea6a37a7e00c89042537e9b
Umberto81 <goffredo.gonnella@gail.com>
2022-03-17 15:27:56 +0000

    updated api request

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import data from '../data';
+import React, { useEffect, useState } from 'react'
+//import data from '../data';
 import Product from '../components/Product';
+import axios from 'axios';
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      const result = await axios.get("/api/products");
+      setProducts(result.data);
+    }
+    fetchData();
+
+  }, [])
+  
   return (
     <div className="row center">
-          {data.products.map((product) => (
+          {products.map((product) => (
             <Product key={product.slug} product={product}></Product>
           ))}
           ;
