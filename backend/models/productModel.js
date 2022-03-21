aba7d2cf21a6245728e3d409833d3512388143a1
Umberto81 <goffredo.gonnella@gail.com>
2022-03-21 15:37:36 +0000

    updated images

---
+++ backend/models/productModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema(
+    {
+    name: { type: String, required: true, unique: true},
+    slug: { type: String, required: true, unique: true},
+    image: { type: String, required: true},
+    brand: { type: String, required: true},
+    category: { type: String, required: true},
+    description: { type: String, required: true},
+    name: { type: String, required: true, unique: true},
+    price: {type: Number, required: true},
+    rating: {type: Nuumber, required: true},   
+    numReviews: {type: Nuumber, required: true},
+},
+    {
+        timestamps: true
+    }
+);
+
+const Product = mongoose.model('Product', ProductSchema);
+export default Product;
--- src/data.js
+++ src/data.js
@@ -1,73 +1,88 @@
 const data = {
-    products: [
-      {
-        name: "Nike Shirt",
-        slug: "nike-shirt",
-        category: "Shirts",
-        image: "./images/p1.jpg",
-        price: 120,
-        brand: "Nike",
-        rating: 4.5,
-        numReviews: 10,
-        description: "High quality product",
-      },
-      {
-        name: "Adidas fit Shirt",
-        slug: "adidas-fit-shirt",
-        category: "Shirts",
-        image: "./images/p1.jpg",
-        price: 120,
-        brand: "Adidas",
-        rating: 4.0,
-        numReviews: 10,
-        description: "High quality product",
-      },
-      {
-        name: "Lacoste Shirt",
-        slug: "lacoste-shirt",
-        category: "Shirts",
-        image: "./images/p1.jpg",
-        price: 300,
-        brand: "Lacoste",
-        rating: 2.5,
-        numReviews: 5,
-        description: "High quality product",
-      },
-      {
-        name: "Pants Adidas",
-        slug: "adidas-pants",
-        category: "Pants",
-        image: "./images/p1.jpg",
-        price: 80,
-        brand: "Adidas",
-        rating: 2.5,
-        numReviews: 3,
-        description: "High quality product",
-      },
-      {
-        name: "Puma",
-        slug: "puma-shirt",
-        category: "Pants",
-        image: "./images/p1.jpg",
-        price: 80,
-        brand: "Puma",
-        rating: 4.5,
-        numReviews: 10,
-        description: "High quality product",
-      },
-      {
-        name: "Adidas Shirt",
-        slug: "adidas-shirt",
-        category: "Shirts",
-        image: "./images/p1.jpg",
-        price: 350,
-        brand: "Adidas",
-        rating: 4.5,
-        numReviews: 20,
-        description: "High quality product",
-      },
-    ],
-  };
-  
-  export default data;
-  
+  products: [
+    {
+      _id:'1',
+      name: "Nike Shirt",
+      slug: "nike-shirt",
+      category: "Shirts",
+      image: "../images/p1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High quality product",
+      countInStock: 2
+    },
+    {
+      _id:'2',
+      name: "Adidas fit Shirt",
+      slug: "adidas-fit-shirt",
+      category: "Shirts",
+      image: "../images/p5.jpg",
+      price: 120,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "High quality product",
+      countInStock: 4
+
+    },
+    {
+      _id:'3',
+      name: "Lacoste Shirt",
+      slug: "lacoste-shirt",
+      category: "Shirts",
+      image: "../images/p1.jpg",
+      price: 300,
+      brand: "Lacoste",
+      rating: 2.5,
+      numReviews: 5,
+      description: "High quality product",
+      countInStock: 0
+
+    },
+    {
+      _id:'4',
+      name: "Pants Adidas",
+      slug: "adidas-pants",
+      category: "Pants",
+      image: "../images/p3.jpg",
+      price: 80,
+      brand: "Adidas",
+      rating: 2.5,
+      numReviews: 3,
+      description: "High quality product",
+      countInStock: 20
+
+    },
+    {
+      _id:'5',
+      name: "Puma",
+      slug: "puma-shirt",
+      category: "Pants",
+      image: "../images/p4.jpg",
+      price: 80,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High quality product",
+      countInStock: 5
+
+    },
+    {
+      _id:'6',
+      name: "Adidas Shirt",
+      slug: "adidas-shirt",
+      category: "Shirts",
+      image: "../images/p2.jpeg",
+      price: 350,
+      brand: "Adidas",
+      rating: 4.5,
+      numReviews: 20,
+      description: "High quality product",
+      countInStock: 0
+    },
+  ],
+};
+
+export default data;
