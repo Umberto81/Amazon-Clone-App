745c28781cffef46aac2270ae1e8e68174579653
Umberto81 <goffredo.gonnella@gail.com>
2022-03-22 18:56:41 +0000

    fixed typos in store

--- src/Store.js
+++ src/Store.js
@@ -4,13 +4,19 @@ import { createContext, useReducer } from "react";
 export const Store = createContext();
 
 const initialState = {
-  userInfo: localStorage.getItem("userInfo")
-    ? JSON.parse(localStorage.getItem("userInfo"))
+  userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
     : null,
+
   cart: {
-    shippingAddress: localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {},
-    cartItems: localStorage.getItem("cartItems")
-      ? JSON.parse(localStorage.getItem("cartItems"))
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : { location: {} },
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : '',
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
   },
 };
@@ -47,10 +53,14 @@ function reducer(state, action) {
     }
 
     case "USER_SIGNOUT": {
-      return { ...state, userInfo: null, cart:{
-        cartItems: [],
-        shippingAddress: {},
-      } };
+      return {
+        ...state,
+        userInfo: null,
+        cart: {
+          cartItems: [],
+          shippingAddress: {},
+        },
+      };
     }
     case "SAVE_SHIPPING_ADDRESS": {
       return {
