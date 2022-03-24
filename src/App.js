14bd7fd1dd55b62031397466e5b69ff0e9d97caa
Umberto81 <goffredo.gonnella@gail.com>
2022-03-24 17:27:54 +0000

    created signup route and component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignInScreen from "./screens/SignInScreen";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import SignUpScreen from "./screens/SignUpSCreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -82,6 +83,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />}></Route>
               <Route path="/cart" element={<CartScreen />}></Route>
               <Route path="/signin" element={<SignInScreen />}></Route>
+              <Route path="/signup" element={<SignUpScreen />}></Route>
               <Route path="/shipping" element={<ShippingAddressScreen />}></Route>
 
             </Routes>
